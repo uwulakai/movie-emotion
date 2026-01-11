cf5773d62b70274f8387cb8321e70f0af7b6d5ed
Черемисин Максим Ильич <thewaymyfeetslippedoftheroof@gmail.com>
2026-01-11 18:10:44 +0500

    Отмена интеграции с S3, поскольку нет бесплатных S3 хранилищ

--- movie_emotion/config.py
+++ movie_emotion/config.py
@@ -43,17 +43,6 @@ class EmailSettings(BaseSettingsConfig):
     EMAIL_HOST_PASSWORD: SecretStr
 
 
-class S3Settings(BaseSettingsConfig):
-    """Настройки подключения S3-клиента"""
-
-    S3_BUCKET_NAME: SecretStr
-    S3_BUCKET_URL: str = "https://storage.clo.ru"
-    S3_ENDPOINT_URL: str = "https://storage.clo.ru:443"
-    S3_ACCESS_KEY: SecretStr
-    S3_SECRET_KEY: SecretStr
-    S3_SIGNATURE_VERSION: str = "s3v4"
-
-
 class Settings(BaseSettings):
     """Общий класс настроек"""
 
